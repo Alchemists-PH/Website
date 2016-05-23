fd0383b73ce1c05e2d9c0799969b6a4041bdc582
Ria Cataquian <icataquian@gmail.com>
2016-05-24 03:47:20 +0800

    Tries to fix travis build

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,4 @@
 ExUnit.start
 
-Mix.Task.run "ecto.create", ~w(-r Website.Repo --quiet)
-Mix.Task.run "ecto.migrate", ~w(-r Website.Repo --quiet)
 Ecto.Adapters.SQL.Sandbox.mode(Website.Repo, :manual)
 
