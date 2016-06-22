2ca8fa9c40ea78c88baa1d1c85829dc1fe2e7ba5
Samantha Rose Cruz <samrosecruz@gmail.com>
2016-06-23 00:32:05 +0800

    Adds close button on sidebar as separate element

--- web/static/js/behaviors/nav-toggle.js
+++ web/static/js/behaviors/nav-toggle.js
@@ -1,19 +1,21 @@
-onmount('[role~="nav-toggle"]', function () {
-  const icon = $(this)
-  const listIcon = $('[role~="list-icon"')
+onmount('[role~="nav-open"]', function () {
+  const iconOpen = $(this)
+  const iconClose = $('[role~="nav-close"')
   const menu = $('[role~="sidebar-menu"')
   const overlay = $('[role~="sidebar-overlay"')
 
-  icon.on('click', function() {
-    menu.toggleClass('-show')
+  iconOpen.on('click', function() {
+    menu.addClass('-show')
     overlay.toggleClass('-show')
-    listIcon.toggleClass('-triggered')
+  })
+
+  iconClose.on('click', function() {
+    menu.removeClass('-show')
+    overlay.toggleClass('-show')
   })
 
   overlay.on('click', function() {
-    menu.toggleClass('-show')
+    menu.removeClass('-show')
     overlay.toggleClass('-show')
-    listIcon.toggleClass('-triggered')
   })
-
 })
