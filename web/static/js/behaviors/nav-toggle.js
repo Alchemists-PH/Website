7de8fa786e99944cd7c87c20ea2309d3debd278b
Samantha Rose Cruz <samrosecruz@gmail.com>
2016-06-23 00:11:09 +0800

    Styles sidenav and adds show/hide state

--- web/static/js/behaviors/nav-toggle.js
+++ web/static/js/behaviors/nav-toggle.js
@@ -1,4 +1,19 @@
 onmount('[role~="nav-toggle"]', function () {
-  console.log('wat')
+  const icon = $(this)
+  const listIcon = $('[role~="list-icon"')
+  const menu = $('[role~="sidebar-menu"')
+  const overlay = $('[role~="sidebar-overlay"')
+
+  icon.on('click', function() {
+    menu.toggleClass('-show')
+    overlay.toggleClass('-show')
+    listIcon.toggleClass('-triggered')
+  })
+
+  overlay.on('click', function() {
+    menu.toggleClass('-show')
+    overlay.toggleClass('-show')
+    listIcon.toggleClass('-triggered')
+  })
 
 })
